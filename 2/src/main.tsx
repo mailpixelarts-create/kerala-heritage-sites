b54c9a3c93208a9456b1ce2963c58ba535578bc1
mailnormanjames-alt <mailnormanjames@gmail.com>
2026-07-16 22:44:13 +0530

    Add Lenis 1.3.24 smooth scroll to all projects

--- 2/src/main.tsx
+++ 2/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Lenis from "lenis";
 import "./index.css";
 import App from "./App";
+
+new Lenis({ autoRaf: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
